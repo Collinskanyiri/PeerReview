67745b1733f215cf9bdb7431f25ced5fafce45f0
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-11-01 14:22:36 +0300

    review site register

--- back-end/backend/admin.py
+++ back-end/backend/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from backend.models import Profile,Project,BlogPost,Like, Review
+from backend.models import Profile,Project,Like, Review
+
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Project)
-admin.site.register(BlogPost)
 admin.site.register(Like)
 admin.site.register(Review)
