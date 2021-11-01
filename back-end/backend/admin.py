ef10b8eba164f365b61c0e3ee0f2454d4b79fb40
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-11-01 12:52:23 +0300

    add site registers

---
+++ back-end/backend/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from backend.models import Profile,Project,BlogPost,Like, Review
+
+# Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(BlogPost)
+admin.site.register(Like)
+admin.site.register(Review)
